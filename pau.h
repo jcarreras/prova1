035a8e58d7180de7ba956dec184ebf9f83049094
Joan Carreras <jcarreras@iesjoanramis.org>
2014-08-20 00:25:49 +0100

    Usuari joan carreras canvia pau.h per veure com funcona
    L-altre ho veu i flipa, perqu[e no sap qu[e 'es i d-on surt

--- pau.h
+++ pau.h
@@ -2,3 +2,5 @@ aix[o 'es una prova
 per modicar .h
 aix'i que ser[a un 6 commit
 
+Farem un nou canvi perqu[e l-altre ho vegi
+
---
+++ pau.h~
@@ -0,0 +1,4 @@
+aix[o 'es una prova
+per modicar .h
+aix'i que ser[a un 6 commit
+
